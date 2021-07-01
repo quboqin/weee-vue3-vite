6e2a6b2c6288c5bfecdbc5144b782aa7e8516927
Qubo Qin <qubo.qin.2018@gmail.com>
2021-07-01 21:09:38 +0800

    add swiper

---
+++ src/components/Swiper.vue
@@ -0,0 +1,39 @@
+<template>
+  <van-swipe class="home-swipe" :autoplay="3000" indicator-color="#1baeae">
+    <van-swipe-item v-for="(item, index) in my_list" :key="index">
+      <img :src="item.carouselUrl" alt="" @click="onClick(item.redirectUrl)" />
+    </van-swipe-item>
+  </van-swipe>
+</template>
+
+<script lang="ts">
+import { defineComponent, toRefs } from 'vue'
+
+export default defineComponent({
+  name: 'swiper',
+  props: {
+    list: Array,
+  },
+  setup(props) {
+    const my_list = toRefs(props).list
+
+    function onClick(url: string) {
+      window.open(url)
+    }
+    return {
+      onClick,
+      my_list,
+    }
+  },
+})
+</script>
+
+<style lang="less" scoped>
+.home-swipe {
+  margin: 116px 0;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
-import { Icon, Button, Search } from 'vant'
+import { Icon, Button, Search, Swipe, SwipeItem } from 'vant'
 import App from './App.vue'
 import router from './router'
 import 'vant/lib/index.css'
 
 const app = createApp(App)
-app.use(Icon).use(Button).use(Search)
+app.use(Icon).use(Button).use(Search).use(Swipe).use(SwipeItem)
 app.use(router)
 app.mount('#app')
