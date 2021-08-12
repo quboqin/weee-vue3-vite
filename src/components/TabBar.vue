cae22f219d770de43fee59ff1e98bb483e71158a
Qubo Qin <qubo.qin.2018@gmail.com>
2021-08-12 14:23:13 +0800

    add material design icon

--- src/components/TabBar.vue
+++ src/components/TabBar.vue
@@ -11,10 +11,8 @@
       bg-yellow-100
     "
   >
-    <router-link v-for="tab in tabs" :key="tab.to" :to="tab.to" flex flex-col>
-      <div>
-        <home-icon className="h-1 w-1 text-blue-500" />
-      </div>
+    <router-link v-for="tab in tabs" :key="tab.to" :to="tab.to" class="py-1">
+      <p :class="tab.icon" class="material-icons md-32 text-center"></p>
       <div>
         <span>{{ tab.title }}</span>
       </div>
@@ -24,16 +22,14 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { HomeIcon } from '@heroicons/vue/solid'
 
 export default defineComponent({
   name: 'tabbar',
-  components: { HomeIcon },
   setup() {
     const tabs = [
-      { icon: 'wap-home', to: 'home', title: '首页' },
-      { icon: 'cluster', to: 'category', title: '分类' },
-      { icon: 'manager', to: 'profile', title: '我的' },
+      { icon: 'mdi mdi-home', to: 'home', title: '首页' },
+      { icon: 'mdi mdi-magnify', to: 'category', title: '分类' },
+      { icon: 'mdi mdi-account', to: 'profile', title: '我的' },
     ]
 
     return {
@@ -43,36 +39,4 @@ export default defineComponent({
 })
 </script>
 
-<style lang="less" scoped>
-.tab-bar {
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  padding: 5px 0;
-  z-index: 1000;
-  background: #fff2f2;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: row;
-  padding: 5px 0px;
-  .tab-bar-item {
-    flex: auto;
-    display: flex;
-    flex-direction: column;
-    text-align: center;
-    color: #1baeae;
-    &.router-link-active {
-      color: #666;
-    }
-    i {
-      text-align: center;
-      font-size: 22px;
-    }
-    span {
-      font-size: 12px;
-    }
-  }
-}
-</style>
+<style lang="less" scoped></style>
